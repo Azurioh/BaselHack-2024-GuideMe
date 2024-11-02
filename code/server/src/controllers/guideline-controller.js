0f750e1dbe6921e678fa2defab0fbb9e77f4c458
glaentzlin kevin <kevin1.glaentzlin@epitech.eu>
2024-11-02 15:00:44 +0100

    [+] add latex and custom generate pdf

--- code/server/src/controllers/guideline-controller.js
+++ code/server/src/controllers/guideline-controller.js
@@ -1,3 +1,11 @@
+import fs from 'fs';
+import path from 'path';
+import { exec } from 'child_process';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export class GuidelineController {
   constructor(guidelineService) {
     this.guidelineService = guidelineService;
@@ -64,6 +72,50 @@ export class GuidelineController {
     } catch (err) {
       console.error(err);
       res.status(500).json({ err: 'Internal server error.' });
+    }
+  };
+
+  generatePdfFromMarkdown = async (req, res) => {
+    try {
+      const { markdownContent, title } = req.body;
+
+      if (!markdownContent) {
+        return res.status(400).json({ err: 'Contenu Markdown requis.' });
+      }
+
+      const markdownFilePath = path.join(__dirname, 'temp.md');
+      const pdfFilePath = path.join(__dirname, 'output.pdf');
+      const templatePath = path.join(__dirname, 'custom-template.tex');
+
+      const content = `---
+  title: "${title}"
+  ---
+
+  ${markdownContent}`;
+
+      fs.writeFileSync(markdownFilePath, content);
+
+      const command = `pandoc ${markdownFilePath} --pdf-engine=xelatex --template=${templatePath} --resource-path=${__dirname} -o ${pdfFilePath}`;
+
+      exec(command, (error) => {
+        if (error) {
+          console.error('Erreur lors de la conversion :', error);
+          return res.status(500).json({ err: 'Erreur de conversion en PDF.' });
+        }
+
+        res.download(pdfFilePath, 'output.pdf', (err) => {
+          if (err) {
+            console.error("Erreur lors de l'envoi du PDF :", err);
+            return res.status(500).json({ err: 'Erreur lors du téléchargement du PDF.' });
+          }
+
+          fs.unlinkSync(markdownFilePath);
+          fs.unlinkSync(pdfFilePath);
+        });
+      });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ err: 'Erreur serveur.' });
     }
   };
 }
