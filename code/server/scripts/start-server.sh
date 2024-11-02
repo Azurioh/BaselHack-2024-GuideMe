724f838eedfaa904f9bdc5121709db3cb370fe7e
Azurioh <alan.cunin@epitech.eu>
2024-11-02 11:26:06 +0100

    refactor(server): Update start-server script for PROD deployement

--- code/server/scripts/start-server.sh
+++ code/server/scripts/start-server.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-if [ "$LOCAL" = "false" ]; then
-    export DATABASE_HOSTNAME="$PROD_HOSTNAME"
+if [ "$PROD" = "true" ]; then
+    export DATABASE_HOSTNAME="baselhack.azu-dev.fr"
 fi
+
+export DATABASE_URL=postgresql://postgres:root@$DATABASE_HOSTNAME:5432/baselhack?schema=public
 
 if [ "$MIGRATE" = "true" ]; then
     npx prisma migrate dev --name "initial_migration" --schema=./prisma/schema.prisma
