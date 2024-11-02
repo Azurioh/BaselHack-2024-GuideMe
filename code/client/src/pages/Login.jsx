75257734b68ba0dd659402b2acadab2244840b6e
Antoine Grand <antoine.grand@epitech.eu>
2024-11-02 11:10:44 +0100

    [Feature][+] = Add the form in the login and signup pages. File modified : src/pages/Login.jsx, src/pages/Signup.jsx

--- code/client/src/pages/Login.jsx
+++ code/client/src/pages/Login.jsx
@@ -1,19 +1,53 @@
-import { Button } from 'antd';
+import { Button, Flex, Form, Input } from 'antd';
 import { useAuth } from '../auth/AuthContext';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+
 
 function Login() {
     const {login} = useAuth();
+    const navigate = useNavigate();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleLogin() {
+        login();
+        console.log("email: ", email);
+        console.log("password: ", password);
+        // TODO: call the api to login
+        navigate('/');
+    }
 
     return (
-        <div>
-            <h1>Login</h1>
-            <Button
-                type='primary'
-                onClick={() => login()}
+        <Flex>
+            <Form
+                layout='vertical'
+                onFinish={handleLogin}
+                initialValues={{ remember: true }}
             >
-                Login
-            </Button>
-        </div>
+                <Form.Item
+                    label='Email'
+                    name='email'
+                    rules={[{ required: true, message: 'Please input your email!', type: 'email' }]}
+                >
+                    <Input onChange={(e) => setEmail(e.target.value)} />
+                </Form.Item>
+                <Form.Item
+                    label='Password'
+                    name='password'
+                    rules={[{ required: true, message: 'Please input your password!'}]}
+                >
+                    <Input.Password onChange={(e) => setPassword(e.target.value)}/>
+                </Form.Item>
+                <Button
+                    type='primary'
+                    htmlType='submit'
+                >
+                    Login
+                </Button>
+            </Form>
+        </Flex>
     )
 }
 
