180b9bad5e3be66cef8ffe8e87abe92879658ed2
Azurioh <alan.cunin@epitech.eu>
2024-11-02 04:42:40 +0100

    feat(server): Manage not found routes

--- code/server/src/global.js
+++ code/server/src/global.js
@@ -16,3 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
 app.use('/users', userRouter);
+
+app.use('/*', (req, res) => {
+  res.status(404).json({ err: 'Route not found.' });
+});
