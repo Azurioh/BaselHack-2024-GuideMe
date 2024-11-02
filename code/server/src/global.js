dee6b5a3c53a3b6cb04d65c48ac8fb1cf8f64af3
Azurioh <alan.cunin@epitech.eu>
2024-11-02 16:01:41 +0100

    try to fix cors

--- code/server/src/global.js
+++ code/server/src/global.js
@@ -4,7 +4,6 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { PrismaClient } from '../prisma/client/index.js';
 import userRouter from './routers/user-router.js';
-import authRouter from './routers/auth-router.js';
 import guidelineRouter from './routers/guideline-router.js';
 
 dotenv.config();
@@ -12,24 +11,22 @@ dotenv.config();
 export const app = express();
 export const prisma = new PrismaClient();
 
-const corsOptions = {
-  origin: ['http://localhost:3000', 'https://baselhack.azu-dev.fr'],
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  optionsSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use((_, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.get('/', (_, res) => {
   res.status(200).json({ data: 'API is ready to use!' });
 });
 
 app.use('/users', userRouter);
-app.use('/auth', authRouter);
 app.use('/guidelines', guidelineRouter);
 
 app.use('/*', (req, res) => {
