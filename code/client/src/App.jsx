183587d9908585e37f541b43782f7bc807374546
Antoine Grand <antoine.grand@epitech.eu>
2024-11-02 02:00:30 +0100

    [Feature][+] = Add protection in the protected route. File modified : package-lock.json, src/App.jsx, src/main.jsx, src/pages/Login.jsx File created : src/auth/, src/components/

--- code/client/src/App.jsx
+++ code/client/src/App.jsx
@@ -4,11 +4,12 @@ import Login from './pages/Login'
 import { Routes, Route } from 'react-router-dom'
 import Signup from './pages/Signup'
 import NotFound from './pages/NotFound'
+import AuthorizedRoute from './components/AuthorizedRoute'
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home/>} />
+      <Route path='/' element={<AuthorizedRoute><Home/></AuthorizedRoute>} />
       <Route path='/login' element={<Login/>} />
       <Route path='/signup' element={<Signup/>} />
       <Route path='/*' element={<NotFound/>} />
